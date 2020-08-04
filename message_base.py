278db5d0baf03e8805c5b47bc2416f202015b732
Muhammed Zubair <47239508+ZanZubair96@users.noreply.github.com>
2020-08-05 02:24:08 +0530

    Update message_base.py
    Updated privacy

--- message_base.py
+++ message_base.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client 
 
  
-account_sid = 'ACe52fa3d12ad98e8ace01ceb42ae37d85' 
-auth_token = 'c82d964a909cd0df71ec431b0a39c8e1' 
+account_sid = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  #use the account_sid from the Twilio app
+auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  #use the auth_token from the Twilio app
 client = Client(account_sid, auth_token) 
  
 def send_love_message():
   message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
                               body='Good Night by ZAN 😍',    
-                              to='whatsapp:+918190899870' 
+                              to='whatsapp:+91{YOUR_NUMBER}' 
                           ) 
  
   print(message.sid)
